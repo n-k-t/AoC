330285bfd27363769644e4c39f14568781506a75
n-k-t <nkt8@case.edu>
2022-12-19 23:34:51 -0500

    Finished the first problem of day 1, 2015.

---
+++ AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/debug/deps/aoc2015_d01_a01-dc60a7ae6827afd9.d
@@ -0,0 +1,5 @@
+/home/ntomczak/Documents/Git/AoC/AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/debug/deps/aoc2015_d01_a01-dc60a7ae6827afd9: src/main.rs
+
+/home/ntomczak/Documents/Git/AoC/AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/debug/deps/aoc2015_d01_a01-dc60a7ae6827afd9.d: src/main.rs
+
+src/main.rs:
---
+++ AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/release/deps/aoc2015_d01_a01-b8fb41d347a979a6.d
@@ -0,0 +1,5 @@
+/home/ntomczak/Documents/Git/AoC/AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/release/deps/aoc2015_d01_a01-b8fb41d347a979a6: src/main.rs
+
+/home/ntomczak/Documents/Git/AoC/AoC-2015/day-01/attempt-01/aoc2015-d01-a01/target/release/deps/aoc2015_d01_a01-b8fb41d347a979a6.d: src/main.rs
+
+src/main.rs:
